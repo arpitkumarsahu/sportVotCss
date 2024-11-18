f1b384bb2e8bc4b11d7f946c72374b8a66ff0bbd
Arpit Kumar Sahu <arpit.ks@nuveb.com>
2024-11-18 16:28:48 +0530

    infinite scroll

--- src/App.js
+++ src/App.js
@@ -6,13 +6,32 @@ const App = () => {
   const boxes = Array.from({ length: 7 }, (_, index) => index + 1); // Create 7 boxes
 
   return (
-    <div className="box-container">
+    <div>
+<div className="box-container">
       {boxes.map((box, index) => (
         <div key={index} className="box">
           Box {box}
         </div>
       ))}
     </div>
+
+
+    <div className="infinite-scroll">
+      <div className="scroll-content">
+        <div className="item">Item 1</div>
+        <div className="item">Item 2</div>
+        <div className="item">Item 3</div>
+        <div className="item">Item 4</div>
+        <div className="item">Item 5</div>
+        <div className="item">Item 1</div>
+        <div className="item">Item 2</div>
+        <div className="item">Item 3</div>
+        <div className="item">Item 4</div>
+        <div className="item">Item 5</div>
+      </div>
+    </div>
+    </div>
+    
    
   );
 };
